9cb28af5e04e08ce0e84da1c8126af1c996fb168
Marina Shvabauer <schwmk@gmail.com>
2021-06-09 23:11:32 +0300

    add: add Htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,8 @@ function MyApp({Component, pageProps}: AppProps): JSX.Element {
             <Head>
                 <title>My Top App</title>
                 <link rel="icon" href="/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
             </Head>
             <Component {...pageProps} />
         </>
