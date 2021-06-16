0004c57b39b241a4512053f687a8edd98a2b6bc1
Marina Shvabauer <schwmk@gmail.com>
2021-06-16 23:19:18 +0300

    add: add paragraph

---
+++ components/Paragraph/Paragraph.tsx
@@ -0,0 +1,20 @@
+import cn from 'classnames';
+
+import {IParagraphProps} from "./Paragraph.props";
+
+import styles from './Paragraph.module.css';
+
+export const Paragraph = ({appearance = 'base', children, className, ...props}: IParagraphProps): JSX.Element => {
+    return (
+        <p className={cn(styles.paragraph, className, {
+            [styles.base]: appearance === 'base',
+            [styles.small]: appearance === 'small',
+            [styles.big]: appearance === 'big',
+        })}
+            {...props}
+        >
+            {children}
+        </p>
+    );
+};
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import {Button, Htag} from "../components/";
+import {Button, Htag, Paragraph} from "../components/";
 
 export default function Home(): JSX.Element {
     return (
         <div>
             <Htag tag='h1'>Hello</Htag>
+            <Paragraph appearance='big'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolorem doloremque doloribus
+                eius excepturi exercitationem hic impedit ipsa laborum magni nemo nulla obcaecati, provident quia totam.
+                Dignissimos possimus sapiente temporibus?</Paragraph>
+            <Paragraph>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolorem doloremque doloribus
+                eius excepturi exercitationem hic impedit ipsa laborum magni nemo nulla obcaecati, provident quia totam.
+                Dignissimos possimus sapiente temporibus?</Paragraph>
+            <Paragraph appearance='small'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolorem doloremque doloribus
+                eius excepturi exercitationem hic impedit ipsa laborum magni nemo nulla obcaecati, provident quia totam.
+                Dignissimos possimus sapiente temporibus?</Paragraph>
             <Button appearance='primary' arrow='right'>Button</Button>
             <Button appearance='primary'>Button</Button>
             <Button appearance='ghost'>Button</Button>
